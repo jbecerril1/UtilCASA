9b2a58ca5f381d7b36bae705055af4293f32890c
Julio Becerril <jbecerril@sistemascasa.com.mx>
2022-02-01 11:22:13 -0600

    Proyecto de administración de base de datos Sistemas CASA
    Administrar bases de datos

---
+++ UtilCASA_V2.dpr
@@ -0,0 +1,14 @@
+program UtilCASA_V2;
+
+uses
+  Vcl.Forms,
+  uFm_Princ in 'uFm_Princ.pas' {Fm_Princ};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFm_Princ, Fm_Princ);
+  Application.Run;
+end.
