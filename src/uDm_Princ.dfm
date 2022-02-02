74997c47e7f6f682e8db30804c5649de8ca24f12
Julio Becerril <jbecerril@sistemascasa.com.mx>
2022-02-02 17:26:35 -0600

    EN Linea correctamente

--- src/uDm_Princ.dfm
+++ src/uDm_Princ.dfm
@@ -2,4 +2,25 @@ object Dm_Princ: TDm_Princ
   Height = 480
   Width = 640
   PixelsPerInch = 96
+<<<<<<< Updated upstream
+=======
+  object con_SqLite: TFDConnection
+    ConnectionName = 'UtilCasaSQLite'
+    Params.Strings = (
+      'Database=F:\Proyectos11\UtilCASA\Win32\Debug\UtilCASA.db'
+      'DriverID=SQLite')
+    Left = 104
+    Top = 24
+  end
+  object fdgxwtcrsr1: TFDGUIxWaitCursor
+    Provider = 'Forms'
+    Left = 312
+    Top = 296
+  end
+  object FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink
+    VendorLib = 'sqlite3.dll'
+    Left = 248
+    Top = 24
+  end
+>>>>>>> Stashed changes
 end
--- src/uDm_Princ.pas
+++ src/uDm_Princ.pas
@@ -3,10 +3,19 @@ unit uDm_Princ;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, uLib_UtilCASA, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client, FireDAC.Stan.ExprFuncs,
+  FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.Phys.SQLiteDef, FireDAC.Phys.SQLite,
+  FireDAC.Comp.UI;
 
 type
   TDm_Princ = class(TDataModule)
+    con_SqLite: TFDConnection;
+    fdgxwtcrsr1: TFDGUIxWaitCursor;
+    FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink;
+    procedure DataModuleCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,5 +30,26 @@ implementation
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 
 {$R *.dfm}
+
+procedure TDm_Princ.DataModuleCreate(Sender: TObject);
+begin
+/// <summary>
+///Inicializacion del sistema
+/// </summary>
+  Proyecto.Siglas := 'UtilCASA';
+/// <summary>
+///   Configuración de base de datos SQLite
+/// </summary>
+  try
+    FDPhysSQLiteDriverLink1.VendorHome := Proyecto.Ruta;
+    FDPhysSQLiteDriverLink1.VendorLib := 'sqlite3.dll';
+    con_SqLite.Params.Database := 'UtilCASA.db';
+    con_SqLite.Open();
+  except
+    on e:exception do begin
+      Log('Error al abrir la base de datos: ' + e.Message);
+    end;
+  end;
+end;
 
 end.
